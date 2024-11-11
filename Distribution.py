212bd4db2d005ce8c1d55a15e37dc179406aa96a
Antoine Labalette <labalette.antoine@gmail.com>
2024-11-11 17:42:18 +0100

    part 1: norme + readme

--- Distribution.py
+++ Distribution.py
@@ -5,44 +5,40 @@ import seaborn as sns
 
 
 def render(fruit_type, label, size):
-	fig, (ax1, ax2) = plt.subplots(1, 2)
-	fig.suptitle(fruit_type + " class distribution")
-	palette = sns.color_palette("Set2")
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    fig.suptitle(fruit_type + " class distribution")
+    palette = sns.color_palette("Set2")
 
-	ax1.pie(size, labels=label, colors= palette, autopct='%1.1f%%')
+    ax1.pie(size, labels=label, colors=palette, autopct='%1.1f%%')
 
-	plt.xticks(rotation=60)
-	ax2.bar(label, size, color=palette)
+    plt.xticks(rotation=60)
+    ax2.bar(label, size, color=palette)
 
-	plt.tight_layout()
+    plt.tight_layout()
 
-	plt.show()
+    plt.show()
+
 
 def is_dir(dirname, d):
     return os.path.isdir(os.path.join(dirname, d))
 
 
-def main(dirname):
-    # dirs = [d for d in os.listdir(dirname) if is_dir(dirname, d)]
-    # dir_size = [len(os.listdir(os.path.join(dirname, d))) for d in dirs]
+def main(root_dir):
+    fruit_dictionary = {}
+    dirnames = [d for d in os.listdir(root_dir) if is_dir(root_dir, d)]
+    for dirname in sorted(dirnames):
 
-	fruit_dictionary = {}
+        fruit = dirname.split("_")[0]
+        if fruit not in fruit_dictionary:
+            fruit_dictionary[fruit] = {"label": [], "size": []}
+        fruit_dictionary[fruit]["label"].append(dirname)
+        size = len(os.listdir(os.path.join(root_dir, dirname)))
+        fruit_dictionary[fruit]["size"].append(size)
 
-	for root, dirs, files in os.walk(dirname):
-		for dir in sorted(dirs):
-			if "_" in dir:
-				dir_path = os.path.join(root, dir)
-				files_in_dir = [item for item in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, item))]
-			
-				if files_in_dir:
-					fruit = dir.split("_")[0]
-					if fruit not in fruit_dictionary:
-						fruit_dictionary[fruit] = {"label": [], "size": []}
-					fruit_dictionary[fruit]["label"].append(dir)
-					fruit_dictionary[fruit]["size"].append(len(files_in_dir))
-
-	for fruit in sorted(fruit_dictionary):
-		render(fruit, fruit_dictionary[fruit]["label"], fruit_dictionary[fruit]["size"])
+    for fruit in sorted(fruit_dictionary):
+        label = fruit_dictionary[fruit]["label"]
+        size = fruit_dictionary[fruit]["size"]
+        render(fruit, label, size)
 
 
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -16,10 +16,14 @@ pip install -r requirements.txt
 ### Part 1: Analysis of the Data Set
 
 ```
+curl https://cdn.intra.42.fr/document/document/17547/leaves.zip --output ~/goinfre/file
+unzip ~/goinfre/file -d ~/goinfre
 python Distribution.py <dirname>
 ```
 
 ### Part 2: Data augmentation
+
+You can change which augmentations to use in the code
 
 To transform 1 specific img:
 ```
