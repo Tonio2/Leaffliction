6224dcb8b78e0706e3087f93bb885964a7a17013
Lisa <lisa.duheron@dauphine.eu>
2024-11-11 13:48:17 +0100

    Part2: add augmentation function

--- Augmentation.py
+++ Augmentation.py
@@ -4,6 +4,28 @@ import sys
 import os
 
 
+def augmentation(src, path):
+
+    # (h, w) = img.shape[:2]
+    # center = (w // 2, h // 2)
+	# ajouter median blur
+	modification = {}
+
+	modification["_Median_Blur.JPG"] = cv2.medianBlur(src, 7)
+	modification["_Blur.JPG"] = cv2.GaussianBlur(src, (7, 7), 10)
+	modification["_Contrast.JPG"] = cv2.convertScaleAbs(src, alpha=0.5)
+	modification["_Flip.JPG"] = cv2.flip(src, 270)
+	modification["_Hue_Adjustment.JPG"] = cv2.cvtColor(src, cv2. COLOR_BGR2HSV)
+	modification["_Illumination.JPG"] = cv2.convertScaleAbs(src, beta=50)
+	modification["_Rotate.JPG"] = cv2.rotate(src, cv2.ROTATE_90_CLOCKWISE) 
+	modification["_Zoom.JPG"] = cv2.resize(src, None, fx=5, fy=5, interpolation=cv2.INTER_NEAREST)
+
+	# save image 
+	for key, value in modification.items():
+		save_path = path.split(".JPG")[0] + key
+		cv2.imwrite(save_path, value)
+
+
 def transform(image_path):
 
     img = cv2.imread(image_path)
