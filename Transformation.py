93a1e3bd0d75fa4dafcfd0e88da34771698de3cf
Lisa <lisa.duheron@dauphine.eu>
2024-11-12 12:18:02 +0100

    Part3: Norm

--- Transformation.py
+++ Transformation.py
@@ -1,5 +1,4 @@
 import sys
-import matplotlib.pyplot as plt
 from plantcv import plantcv as pcv
 
 # Pseudo code
@@ -7,7 +6,6 @@ from plantcv import plantcv as pcv
 # protect error
 # retrieve data
 # apply modification
-
 
 # testing on a single plant first
 
@@ -23,15 +21,15 @@ def transformation(src):
     transformation["Original"] = img
     transformation["black_background_white_object"] = masked_image
     transformation["gaussian"] = pcv.gaussian_blur(masked_image, ksize=(7, 7), sigma_x=0, sigma_y=None)
-    transformation["white_mask"] = pcv.apply_mask(img, mask=masked_image, mask_color='white'))
+    transformation["white_mask"] = pcv.apply_mask(img, mask=masked_image, mask_color='white')
     # transformation["black_mask"] = pcv.apply_mask(img, mask=mask, mask_color='black')
 
-    # Defining ROI 
+    # Defining ROI
     # -> roi = object area should be white an background should be black
     # -> device ???
     # device, roi, roi_hierarchy = pcv.define_roi(img=img, shape='rectangle', device=device, roi=transformation["black_background_white_object"], roi_input='binary', debug=None, adjust=False, x_adj=0, y_adj=0, w_adj=0, h_adj=0)
     # device, roi, roi_hierarchy = define_roi(img, 'rectangle', device, roi=None, roi_input='default', debug="print", adjust=True, x_adj=0, y_adj=0, w_adj=0, h_adj=-925)
-	# transformation["Roi_objects"] = 
+    # transformation["Roi_objects"] =
 
     for key, value in transformation.items():
         pcv.plot_image(value)
